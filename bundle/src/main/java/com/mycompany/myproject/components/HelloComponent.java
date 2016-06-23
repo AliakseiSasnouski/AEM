d79c7e4d79c2d2b54aa349d38ba00bd7dcc3f2d0
AliakseiSasnouski <sasnouski.aliaksei@gmail.com>
2016-06-23 15:54:29 +0300

    Task

--- bundle/src/main/java/com/mycompany/myproject/components/HelloComponent.java
+++ bundle/src/main/java/com/mycompany/myproject/components/HelloComponent.java
@@ -2,31 +2,25 @@ package com.mycompany.myproject.components;
 
 import com.adobe.cq.sightly.WCMUsePojo;
 import com.day.cq.wcm.api.Page;
-import jdk.nashorn.internal.ir.annotations.Reference;
+import javax.jcr.Node;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 
 public class HelloComponent extends WCMUsePojo {
 
-    private String message;
-    private String imagePath;
+    private String title;
 
     @Override
     public void activate() throws Exception {
-        Page currentPage = getCurrentPage();
 
-        get
+        Resource componentResource = getResource();
+        ValueMap vm = componentResource.adaptTo(ValueMap.class);
 
-        imagePath = getResource().getPath();
-
-        Resource resource = currentPage.adaptTo(Resource.class);
-
-        ValueMap vm = resource.adaptTo(ValueMap.class);
-
-        this.message = "message";
+       title = vm.get("titleMy", "");
+        getCurrentPage().getContentResource();
     }
 
-    public String getMessage() {
-        return message;
+    public String getTitle() {
+        return title;
     }
 }
